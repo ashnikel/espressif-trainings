b4b44704a76d7b5185cb5e4fbaed6c7e38f59484
Victor Medvedik <ashnikel@gmail.com>
2023-02-12 13:26:37 +0300

    HTTPS client

--- intro/http-client/examples/http_client.rs
+++ intro/http-client/examples/http_client.rs
@@ -3,14 +3,15 @@ use core::str;
 use bsc::wifi::wifi;
 use embedded_svc::{
     http::{
-        client::{Client, Response, Request, RequestWrite},
-       Status,
-    }, io::Read,
+        client::{Client, Request, RequestWrite, Response},
+        Status,
+    },
+    io::Read,
 };
 
 use esp32_c3_dkc02_bsc as bsc;
-use esp_idf_svc::http::client::{EspHttpClient};
-use esp_idf_sys as _; 
+use esp_idf_svc::http::client::EspHttpClient;
+use esp_idf_sys as _;
 
 #[toml_cfg::toml_config]
 pub struct Config {
@@ -31,9 +32,9 @@ fn main() -> anyhow::Result<()> {
 }
 
 fn get(url: impl AsRef<str>) -> anyhow::Result<()> {
-    // 1. Create a new EspHttpClient. (Check documentation) 
+    // 1. Create a new EspHttpClient. (Check documentation)
     let mut client = EspHttpClient::new_default()?;
-   
+
     // 2. Open a GET request to `url`
     let request = client.get(url.as_ref())?;
 
@@ -44,7 +45,7 @@ fn get(url: impl AsRef<str>) -> anyhow::Result<()> {
 
     let writer = request.into_writer(0)?;
 
-    // 4. Submit our write request and check the status code of the response. 
+    // 4. Submit our write request and check the status code of the response.
     // Successful http status codes are in the 200..=299 range.
 
     let mut response = writer.submit()?;
@@ -56,21 +57,22 @@ fn get(url: impl AsRef<str>) -> anyhow::Result<()> {
     match status {
         200..=299 => {
             // 5. if the status is OK, read response data chunk by chunk into a buffer and print it until done
-            let mut buf = [0_u8;256];
+            let mut buf = [0_u8; 256];
             let mut reader = response.reader();
             loop {
-                if let Ok(size) = Read::read(&mut reader, &mut buf){
-                    if size == 0 { break; }
+                if let Ok(size) = Read::read(&mut reader, &mut buf) {
+                    if size == 0 {
+                        break;
+                    }
                     total_size += size;
                     // 6. try converting the bytes into a Rust (UTF-8) string and print it
                     let response_text = str::from_utf8(&buf[..size])?;
                     println!("{}", response_text);
                 }
-            } 
+            }
         }
         _ => anyhow::bail!("unexpected response code: {}", status),
     }
-            
-    
+
     Ok(())
 }
--- intro/http-client/src/main.rs
+++ intro/http-client/src/main.rs
@@ -1,10 +1,11 @@
 use core::str;
+use std::str::from_utf8;
 
 use bsc::wifi::wifi;
 use embedded_svc::{
     http::{
         client::{Client, Request, RequestWrite, Response},
-        Headers, Status,
+        Status,
     },
     io::Read,
 };
@@ -26,15 +27,21 @@ fn main() -> anyhow::Result<()> {
     let _wifi = wifi(CONFIG.wifi_ssid, CONFIG.wifi_psk)?;
 
     // TODO your code here
-    //get(...)?;
+    get("https://espressif.com")?;
 
     Ok(())
 }
 
 fn get(url: impl AsRef<str>) -> anyhow::Result<()> {
     // 1. Create a new EspHttpClient. (Check documentation)
+    let mut client = EspHttpClient::new(&EspHttpClientConfiguration {
+        use_global_ca_store: true,
+        crt_bundle_attach: Some(esp_idf_sys::esp_crt_bundle_attach),
 
+        ..Default::default()
+    })?;
     // 2. Open a GET request to `url`
+    let request = client.get(url)?;
 
     // 3. Requests *may* send data to the server. Turn the request into a writer, specifying 0 bytes as write length
     // (since we don't send anything - but have to do the writer step anyway)
@@ -42,16 +49,41 @@ fn get(url: impl AsRef<str>) -> anyhow::Result<()> {
     // https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/protocols/esp_http_client.html
     // If this were a POST request, you'd set a write length > 0 and then writer.do_write(&some_buf);
 
-    // let writer = request...;
+    let writer = request.into_writer(0)?;
 
     // 4. Turn the writer into a response and check its status. Successful http status codes are in the 200..=299 range.
 
-    // let response = writer...;
-    // let status = ...;
-    // println!("response code: {}\n", status);
+    let response = writer.into_response()?;
+    let status = response.status();
+    println!("Response code: {}", status);
 
-    // 5. If the status is OK, read response data chunk by chunk into a buffer and print it until done.
-    // 6. Try converting the bytes into a Rust (UTF-8) string and print it.
+    match status {
+        200..=299 => {
+            let mut reader = response.reader();
+            let mut total_bytes = 0;
+            loop {
+                let mut buffer = [0u8; 256];
+                let bytes_read = reader.do_read(&mut buffer)?;
+                if bytes_read == 0 {
+                    break;
+                }
+                total_bytes += bytes_read;
+                let response_string = from_utf8(&buffer[..bytes_read])?;
+                print!("{}", response_string);
+            }
+            println!("\nBytes read: {}", total_bytes);
+        }
+        300..=399 => {
+            todo!()
+        }
+        400..=499 => {
+            todo!()
+        }
+        500..=599 => {
+            todo!()
+        }
+        _ => anyhow::bail!("Unexpected response code: {}", status),
+    }
 
     Ok(())
 }
