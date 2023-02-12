88996a76c8c57e11b6fd99b0cbdeb10bd5b08ebe
Victor Medvedik <ashnikel@gmail.com>
2023-02-12 13:06:32 +0300

    Hello world

---
+++ intro/hello-world/src/main.rs
@@ -0,0 +1,15 @@
+use std::{thread::sleep, time::Duration};
+
+use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
+
+fn main() {
+    // It is necessary to call this function once. Otherwise some patches to the runtime
+    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
+    esp_idf_sys::link_patches();
+
+    println!("Hello, world!");
+
+    loop {
+        sleep(Duration::from_secs(1));
+    }
+}
